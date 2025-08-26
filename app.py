3a06a820a52c189968f476bf0e186970812fef31
chenkham <Chenkhamchowlu@gmail.com>
2025-08-27 00:19:47 +0530

    initial commit

--- app.py
+++ app.py
@@ -955,5 +955,5 @@ def test_spam_score():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='127.0.0.1', port=int(os.environ.get('PORT', 5000)))
 
