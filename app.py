7faa189d315cf3715f62282ec9130c40c23d184c
chenkham <Chenkhamchowlu@gmail.com>
2025-08-26 21:12:59 +0530

    Add gunicorn to requirements

--- app.py
+++ app.py
@@ -939,4 +939,5 @@ def test_spam_score():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
